3ea5b8300111cf711e98229829fb5c00a650a0bc
Matin.h.f81 <matinhosseini795@gmail.com>
2022-04-22 02:15:15 +0430

    initial commit

---
+++ my_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\books\django 3 by example 3rd edition-antonio mele-packt-9781838981952-ebooksworld.ir\my_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
