56f389aa73e971c95e92783b6a7d2a9355e46ef4
Matin.h.f81 <matinhosseini795@gmail.com>
2022-07-18 17:21:04 +0430

    Creating post sharing by email & Creating a comment system

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset()\
-                    .filter(status='published')
+            .filter(status='published')
+
 
 class Post(models.Model):
 
@@ -28,17 +30,35 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                        args=[self.publish.year,
-                        self.publish.month,
-                        self.publish.day, self.slug])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {
         'post': post,
-        'form': form
+        'form': form,
+        'sent': sent
     })
 
 
@@ -33,7 +43,7 @@ class PostListView(ListView):
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 1) # 3 posts in each page
+    paginator = Paginator(object_list, 1)  # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -44,8 +54,8 @@ def post_list(request):
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {
-                 'page': page,
-                 'posts': posts})
+        'page': page,
+        'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -54,4 +64,24 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+        # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form
+        })
