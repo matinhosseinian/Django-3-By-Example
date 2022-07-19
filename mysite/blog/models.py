0f9e59b80a8a46fe7ae127e07cf0fc1ecd997f1b
Matin.h.f81 <matinhosseini795@gmail.com>
2022-07-19 19:48:03 +0430

    Adding the tagging functionality & Retrieving posts by similarity

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -32,6 +32,8 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+    
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
+
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
@@ -41,8 +44,14 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 1)  # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -55,7 +64,9 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {
         'page': page,
-        'posts': posts})
+        'posts': posts,
+        'tag': tag
+    })
 
 
 def post_detail(request, year, month, day, post):
@@ -64,7 +75,7 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-        # List of active comments for this post
+    # List of active comments for this post
     comments = post.comments.filter(active=True)
     new_comment = None
     if request.method == 'POST':
@@ -79,9 +90,19 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {
-            'post': post,
-            'comments': comments,
-            'new_comment': new_comment,
-            'comment_form': comment_form
-        })
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
+    })
